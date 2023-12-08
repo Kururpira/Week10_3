f7c7425a8e031c1d6f2b355e7246858c4e67a6dc
Kururpira <kururpira@inbox.ru>
2023-12-08 16:26:05 +0300

    Задание 3

---
+++ 3.js
@@ -0,0 +1,7 @@
+const image = document.getElementById('black_cat1');
+function addImageBlackCat(){
+    image.src="./assets/img/black_cat1.jpg"
+}
+function addImageBlackCat1(){
+    image.src="./assets/img/black_cat3.jpg"
+}
